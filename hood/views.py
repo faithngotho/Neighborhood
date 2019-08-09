78c8e2242b4f20ca851a1281c66e23ef0bac1a6a
faithngotho <faithwangari248@gmail.com>
2019-08-09 15:42:31 +0300

    add a modified route  on the veiws.py file

--- hood/views.py
+++ hood/views.py
@@ -1,9 +1,4 @@
-from django.shortcuts import render,redirect
-from django.http  import HttpResponse
-import datetime as datetime
-from .  forms import UserRegistrationForm,AddProjectForm,UserProjectForm,AddProjectForm
-from . models import Project,Profile
-from django.http import HttpResponse,Http404,HttpresponseRedirect
+from django.shortcuts import render
 
 
 # Create your views here.
