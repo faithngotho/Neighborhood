eacf3f9fd76131f6ce77aeacdabcff073f49f09c
faithngotho <faithwangari248@gmail.com>
2019-08-14 08:04:49 +0300

    add declared function  to the urls file

--- hood/urls.py
+++ hood/urls.py
@@ -1,8 +1,20 @@
-from django.conf .urls import url
-from .  import views
-
-
+from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
+   url(r'^accounts/profile/$',views.profile,name = 'profile'),
+ 
+   url(r'^$',views.timeline,name = 'home_page'),
+   url(r'^hood/$', views.add_hood, name='add_hood'),
+   url(r'^join(?P<neighbourhood_id>\d+)',views.join, name='join'),
+   url(r'^leave/(?P<neighbourhood_id>\d+)',views.leave, name='leave'),
+   url(r'^one_hood(?P<neighbourhood_id>\d+)',views.hood, name='hood'),
+   url(r'^hood/$', views.add_hood, name='add_hood'),
+   url(r'^upload/$', views.upload_business, name='upload_business'),
+ 
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
